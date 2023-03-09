e22af7e978b7ccf2e86f6f85e50bbfa5338acd17
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-09 21:28:10 +0800

    Lesson2_封装_成员变量和访问修饰符

---
+++ Lesson2_封装_成员变量和访问修饰符/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson2_封装_成员变量和访问修饰符
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

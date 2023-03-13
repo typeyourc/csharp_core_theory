1c395532b46d6bdc4a7de5475edd2a7cf1fe029c
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-13 16:30:47 +0800

    Lesson8 静态类和静态构造函数 done

---
+++ Lesson8_封装_静态类和静态构造函数/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Lesson8_封装_静态类和静态构造函数
+{
+    static class Math
+    {
+        public static float PI;
+
+        static Math()
+        {
+            PI = 3.14f;
+            Console.WriteLine("调用次数");
+        }
+
+        public static float AreaOfCircle(float r)
+        {
+            return PI * r * r;
+        }
+        public static float PeriOfCircle(float r)
+        {
+            return PI * r * 2;
+        }
+        public static float AreaOfRect(float width, float height)
+        {
+            return width * height;
+        }
+        public static float PeriOfRect(float width, float height)
+        {
+            return 2 * (width + height);
+        }
+        public static float Abs(float num)
+        {
+            return num > 0 ? num : (-num);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Abs(-9));
+            Console.WriteLine(Math.AreaOfCircle(2));
+
+            //Math a = new Math();
+
+        }
+    }
+}

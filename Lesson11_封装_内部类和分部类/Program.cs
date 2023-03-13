b81d88941c80ee9f08ee6dcbea12134cff08b01d
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-13 22:34:24 +0800

    Lesson11_封装_内部类和分部类 done

---
+++ Lesson11_封装_内部类和分部类/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson11_封装_内部类和分部类
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

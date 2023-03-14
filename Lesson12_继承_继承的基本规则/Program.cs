9598c9b4b3e1d055f507de240b723fec9b85d0bb
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-14 22:59:09 +0800

    Lesson12 继承的基本原则

---
+++ Lesson12_继承_继承的基本规则/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lesson12_继承_继承的基本规则
+{
+    class People
+    {
+        public string name;
+        public int age;
+
+        public void Speak()
+        {
+            Console.WriteLine("我是{0}", name);
+        }
+    }
+    class Warrior : People
+    {
+        public void Atk()
+        {
+            Console.WriteLine("我可以攻击");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Warrior w = new Warrior();
+            w.name = "战士";
+       
+            w.Speak();
+            w.Atk();
+
+        }
+    }
+}
---
+++ 测试专用/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace 测试专用
+{
+    class MyClass
+    {
+        public int a;
+        public int b;
+        public MyClass[] arrs = new MyClass[] { };
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("测试专用专用程序");
+
+            MyClass myClass = new MyClass();
+            MyClass arr1 = new MyClass();
+            if (myClass.arrs.Length == 0)
+            {
+                Console.WriteLine(myClass.arrs);
+            }
+            
+        }
+    }
+}

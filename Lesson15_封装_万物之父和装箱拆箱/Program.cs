97bd4d02a2666e1c795c48488cfb3d4849190ca8
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-16 00:17:42 +0800

    Lesson15 万物之父 object

---
+++ Lesson15_封装_万物之父和装箱拆箱/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson15_封装_万物之父和装箱拆箱
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

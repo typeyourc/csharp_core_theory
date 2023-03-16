22166b701711b63b9b7ad9aacb4892ea02b22766
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-16 14:39:28 +0800

    Lesson16 密封类 和综合练习

---
+++ Lesson16_继承_密封类/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Lesson16_继承_密封类
+{
+    class Car
+    {
+        public float speed;
+        public float maxSpeed;
+        public int totalPeople;
+        public People[] peoples;
+        public int capacity;
+
+        public Car()
+        {
+            speed = 1.0f;
+            maxSpeed = 8.0f;
+            totalPeople = 0;
+            peoples = new People[] { };
+            capacity = 20;
+        }
+
+        public void Drive()
+        {
+
+        }
+        public void Accident()
+        {
+
+        }
+
+    }
+    class People : Car
+    {
+        public void GoOnCar(People p)
+        {
+            peoples[totalPeople] = p;
+            if (totalPeople <= 29)
+            {
+                ++totalPeople;
+                Console.WriteLine("车上现在{0}人", totalPeople);
+            }
+            else
+            {
+                Console.WriteLine("已经满员");
+            }
+        }
+        public void LeaverCar(People p)
+        {
+            peoples[totalPeople] = null;
+            if (totalPeople >= 1)
+            {
+                --totalPeople;
+                Console.WriteLine("车上现在{0}人", totalPeople);
+            }
+            else 
+            {
+                Console.WriteLine("车上已经没人");
+            }
+        }
+    }
+    class Drivers : People
+    {
+
+    }
+    class Passengers : People
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Car car = new Car();
+            People p1 = new People();
+            p1.GoOnCar(p1);
+
+        }
+    }
+}

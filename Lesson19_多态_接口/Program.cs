3e386b3ab5102c209043cdd8505d3ad9ee9ad41a
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-17 00:49:59 +0800

    lesson 19 多态 接口

---
+++ Lesson19_多态_接口/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson19_多态_接口
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("多态 接口");
+
+
+
+
+        }
+    }
+}

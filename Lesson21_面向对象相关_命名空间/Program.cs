7a4075067a855c01e43c7eea307536f6234074ef
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-19 00:35:42 +0800

    Lesson21 练习题

--- Lesson21_面向对象相关_命名空间/Program.cs
+++ Lesson21_面向对象相关_命名空间/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Lesson21_面向对象相关_命名空间
+﻿using UI;
+namespace UI
+{
+    class Image
+    {
+
+    }
+}
+namespace Graph
+{
+    class Image
+    {
+
+    }
+}
+namespace Lesson21_面向对象相关_命名空间
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //练习一：using的用处，引入某个命名空间
+
+            Image image = new Image();
+            Graph.Image image1 = new Graph.Image();
+
         }
     }
 }

c37efe7d8b4770309d1b051f2f3fd4a46f2b56c6
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-18 21:58:49 +0800

    验证用万物之父object多类型多参数传递

--- 测试专用/Program.cs
+++ 测试专用/Program.cs
@@ -1,37 +1,45 @@
 ﻿namespace 测试专用
 {
-    interface IUSB
+    class Son
     {
-        public abstract void DataTrans();
+
     }
-    abstract class Playback : IUSB
-    {
-        public virtual void DataTrans() { Console.WriteLine("播放设备数据传输"); }
-    }
-    abstract class Storage : IUSB
-    {
-        public virtual void DataTrans() { Console.WriteLine("存储设备数据传输"); }
-    }
-    class PortableHardDrive : Storage
-    {
-        public override void DataTrans() { Console.WriteLine("移动硬盘数据传输"); }
-    }
-    class USBFlashDrive : Storage
-    {
-        public override void DataTrans() { Console.WriteLine("U盘数据传输"); }
-    }
-    class MP3 : Playback
-    {
-        public override void DataTrans() { Console.WriteLine("MP3数据传输"); }
-    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
             //Console.WriteLine("测试专用专用程序");
+            TestFun(1, 2, 3, 4f, 2.3, "123", new Son());
+        }
 
-            PortableHardDrive portableHardDrive = new PortableHardDrive();
-            portableHardDrive.DataTrans();
+        //验证用万物之父object多类型多参数传递
+        static void TestFun(params object[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] is int)
+                {
+                    Console.WriteLine("{0}我是整形参数", array[i]);
+                }
+                else if (array[i] is Single)
+                {
+                    Console.WriteLine("{0}我是单精度类型参数", array[i]);
+                }
+                else if (array[i] is double)
+                {
+                    Console.WriteLine("{0}我是双精度类型参数", array[i]);
+                }
+                else if (array[i] is string)
+                {
+                    Console.WriteLine("{0}我是字符串类型参数", array[i]);
+                }
+                else if (array[i] is Son)
+                {
+                    Console.WriteLine("{0}我是Son类参数", array[i]);
+                }
+
+            }
         }
     }
 }

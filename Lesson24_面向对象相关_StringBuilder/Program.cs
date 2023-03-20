04f3391a0b56b3b4fec054478252a50d5758aa0f
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-20 22:28:40 +0800

    lesson 24 面向对象相关 StringBuilder

---
+++ Lesson24_面向对象相关_StringBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Lesson24_面向对象相关_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            StringBuilder str = new StringBuilder("123456",4);
+            Console.WriteLine(str);
+            str.Replace("1", "x");
+            Console.WriteLine(str);
+
+        }
+    }
+}

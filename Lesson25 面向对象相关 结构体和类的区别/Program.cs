045fcfebe07c10a21eac34b4d66a4aa468b997e8
126074784+typeyourc <126074784+typeyourc@users.noreply.github.com>
2023-03-20 23:16:46 +0800

    Lesson 25&26

---
+++ Lesson25 面向对象相关 结构体和类的区别/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson25_面向对象相关_结构体和类的区别
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Lesson26 面向对象相关 抽象类和接口的区别/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson26_面向对象相关_抽象类和接口的区别
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
